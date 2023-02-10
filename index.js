66de0725bba1ced9da0f6df917ef01b93517705b
z-bradshaw <122636031+z-bradshaw@users.noreply.github.com>
2023-02-10 16:14:44 -0500

    Update index.js

--- index.js
+++ index.js
@@ -52,18 +52,18 @@ const storage = multer.diskStorage({
   app.use("/users", userRoutes);
   app.use("/posts", postRoutes);
   
-  /* MONGOOSE SETUP */
-  const PORT = process.env.PORT || 6001;
-  mongoose
-    .connect(process.env.MONGO_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
-    .then(() => {
-      app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
-  
-      /* ADD DATA ONE TIME */
-      //User.insertMany(users);
-      //Post.insertMany(posts);
-    })
-    .catch((error) => console.log(`${error} did not connect`));
+ /* MONGOOSE SETUP */
+const PORT = process.env.PORT || 6001;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+    /* ADD DATA ONE TIME */
+    // User.insertMany(users);
+    // Post.insertMany(posts);
+  })
+  .catch((error) => console.log(`${error} did not connect`));
